e1a09cde5e8e814584e41141e0fafad0633666c8
Amarnath <120359486+user-kube@users.noreply.github.com>
2024-08-26 08:25:05 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-containerid='docker ps | awk -F " " '{print $1}''
-docker rm -f $containerid
+#containerid='docker ps | awk -F " " '{print $1}''
+#docker rm -f $containerid
 
 # Pull the Docker image from Docker Hub
 docker pull amarnath001/simple-python-flask-app
